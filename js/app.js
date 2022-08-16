131adec43e2a9274221dd76d1f472d1f28d16a67
Beshoy  Nady <60264641+beshoynady@users.noreply.github.com>
2022-08-16 20:35:43 +0200

    Update app.js
    I improved the appearance of the code by using the prefixes for Visual Studio Code and adding comments to it

--- js/app.js
+++ js/app.js
@@ -22,73 +22,86 @@
  * Define Global Variables
  *
  */
-
+//select the sections by TagName
 let sections = document.getElementsByTagName("section");
+//select unordered lists  by ID
 let ul = document.querySelector("#navbar__list");
 
 /**
  * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
+ * 
  * Begin Main Functions
  *
  */
+// build the nav
+// function build the nav and add li dynamically
 function addLi() {
+    //Four lobes to create li with the number of the section
     for (let i = 0; i < sections.length; i++) {
-    let li = document.createElement("li");
-
-    li.innerHTML = `<a class="menu__link" href="#${sections[i].getAttribute(
-    "id")}"> ${sections[i].getAttribute("data-nav")} </a>`;
-    ul.appendChild(li);
+        //Create li
+        let li = document.createElement("li");
+        // Add an anchor for each li
+        li.innerHTML = `<a class="menu__link" href="#${sections[i].getAttribute(
+        "id"
+        )}"> ${sections[i].getAttribute("data-nav")} </a>`;
+        //Add items to unordered lists
+        ul.appendChild(li);
     }
-    
 }
 addLi();
-// build the nav
-// Add class 'active' to section when near top of viewport
 
+
+// Add class 'active' to section when near top of viewport
+//
 window.addEventListener("scroll", function () {
     for (sec of sections) {
-    if (
-        sec.getBoundingClientRect().top < 100 ||
-        sec.getBoundingClientRect().buttom < 400
-    ) {
+        if (
+            //Use the getBoundingClientRect() function to calculate the distance between the section and the top of the page
+        sec.getBoundingClientRect().top < 100 &&
+        sec.getBoundingClientRect().top > -400
+        ) {
         sec.classList.add("your-active-class");
 
-      // Add class 'active' to anchor when click it and near top or bottom of his section viewport
+        // Add class 'active' to anchor when click it or near of his section viewport
         for (a of anchor) {
             if (a.getAttribute("href").slice(1) == sec.getAttribute("id")) {
-                a.classList.add("active");
+            a.classList.add("active");
             } else {
             a.classList.remove("active");
             }
         }
+
         } else {
         sec.classList.remove("your-active-class");
         }
     }
 });
 
-// Scroll to anchor ID using scrollTO event
 
+//#### begin the code Scroll to section smoothly  ######
+//create list of anchor nav 
 let anchor = document.querySelectorAll(".menu__link");
+ // When you click on the link, Default for href is prevented and goes to to section smoothly
 anchor.forEach((a) => {
     a.addEventListener("click", (e) => {
-    e.preventDefault();
+        e.preventDefault();
 
-    let sectoscroll = document.querySelector(a.getAttribute("href"));
+        //Select section by ID associated with Anchor clicked
+        let sectoscroll = document.querySelector(a.getAttribute("href"));
 
-    sectoscroll.scrollIntoView({
+        sectoscroll.scrollIntoView({
         behavior: "smooth",
         block: "start",
         inline: "nearest",
         });
     });
 });
+
+// end the code Scroll to section smoothly
+
+
+
+
 /**
  * End Main Functions
  * Begin Events
